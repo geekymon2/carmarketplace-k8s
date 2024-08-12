51d5990b0edbab8f626eb29892c36b4f137fe72e
geekymon2 <geekymon2@gmail.com>
2024-08-12 13:11:26 +1000

    updated

--- k8s/90-deploy-namespace-kubetail.sh
+++ k8s/90-deploy-namespace-kubetail.sh
@@ -1,1 +1,3 @@
+#!/bin/bash
+
 kubectl create -f ./k8s-namespace/kubetail.yaml
--- k8s/91-deploy-kubetail.sh
+++ k8s/91-deploy-kubetail.sh
@@ -1,1 +1,3 @@
+#!/bin/bash
+
 kubectl apply -f https://github.com/kubetail-org/kubetail/releases/latest/download/kubetail-clusterauth.yaml
