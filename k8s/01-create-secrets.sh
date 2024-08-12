e54be8585958faa6584790d4bae3eb39fc230b03
geekymon2 <geekymon2@gmail.com>
2024-08-12 10:55:53 +1000

    updated

--- k8s/01-create-secrets.sh
+++ k8s/01-create-secrets.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+# NOTE: before running this script make sure to replace placeholders with actual values
+# NOTE: Do not check in this file with secret values
 ENV=$1
 
 # Check mandatory parameter
@@ -7,8 +9,14 @@ if [ -z "$ENV" ]; then
     exit 1
 fi
 
-kubectl create secret generic encryptkeysecret --namespace carmarketplace-$ENV --from-literal encryptkeysecret=r4qBPksanL
-kubectl create secret generic carinfodbpassword --namespace carmarketplace-$ENV --from-literal carinfodbpassword=P@ssw0rd
-kubectl create secret generic carlistingdbpassword --namespace carmarketplace-$ENV --from-literal carlistingdbpassword=P@ssw0rd
-kubectl create secret generic userdbpassword --namespace carmarketplace-$ENV --from-literal userdbpassword=P@ssw0rd
-kubectl create secret generic jwtauthsecret --namespace carmarketplace-$ENV --from-literal jwtauthsecret=zVJrrTE6ba
+kubectl delete secret encryptkeysecret --namespace carmarketplace-$ENV
+kubectl delete secret carinfodbpassword --namespace carmarketplace-$ENV
+kubectl delete secret carlistingdbpassword --namespace carmarketplace-$ENV
+kubectl delete secret userdbpassword --namespace carmarketplace-$ENV
+kubectl delete secret jwtauthsecret --namespace carmarketplace-$ENV
+
+kubectl create secret generic encryptkeysecret --namespace carmarketplace-$ENV --from-literal encryptkeysecret=[ENCRYPT_KEY_SECRET]
+kubectl create secret generic carinfodbpassword --namespace carmarketplace-$ENV --from-literal carinfodbpassword=[CARINFO_DB_PASSWORD]
+kubectl create secret generic carlistingdbpassword --namespace carmarketplace-$ENV --from-literal carlistingdbpassword=[CARLISTING_DB_PASSWORD]
+kubectl create secret generic userdbpassword --namespace carmarketplace-$ENV --from-literal userdbpassword=[USER_DB_PASSWORD]
+kubectl create secret generic jwtauthsecret --namespace carmarketplace-$ENV --from-literal jwtauthsecret=[JWT_AUTH_SECRET]
