9c2bf53d4071d6ea25e67dd0fda1203e0fbc533b
geekymon2 <geekymon2@gmail.com>
2024-08-15 15:54:17 +1000

    updated

--- install-minikube/01-start-minikube.sh
+++ install-minikube/01-start-minikube.sh
@@ -2,6 +2,3 @@
 
 #Start Minikube with virtualbox
 minikube start --cpus=8 --memory=8192 --vm-driver=virtualbox --no-vtx-check
-
-#Start Minikube with docker
-minikube start --cpus=8 --memory=8192 --vm-driver=docker --no-vtx-check
